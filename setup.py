7c185ad9e0999db43e421c7f4c43fcc3334bdffb
Code_Captain <50145701+aman977381@users.noreply.github.com>
2024-04-28 16:13:50 +0530

    Setup file created

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "Waste_Detection",
+    version = '0.0.0',
+    author = "Aman Singh",
+    author_email = "asingh97781@gmail.com",
+    packages = find_packages(),
+    install_requires = []
+)
