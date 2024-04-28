27249e1df4a7c334da636eeb6d7d5b0cf369972e
Code_Captain <50145701+aman977381@users.noreply.github.com>
2024-04-28 18:09:12 +0530

    Data Validation completed

--- Waste_Detection/constant/training_pipeline/__init__.py
+++ Waste_Detection/constant/training_pipeline/__init__.py
@@ -1,3 +1,7 @@
+"""
+Data Ingestion
+"""
+
 ARTIFACTS_DIR: str = "artifacts"
 
 DATA_INGESTION_DIR_NAME: str = "data_ingestion"
@@ -5,3 +9,14 @@ DATA_INGESTION_DIR_NAME: str = "data_ingestion"
 DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
 
 DATA_DOWNLOAD_URL: str = "https://drive.google.com/file/d/1ECfl3dtYyfivY8kYPq7RHUBTjC-2vf61/view?usp=share_link"
+
+"""
+Data Validation
+"""
+
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+
+DATA_VALIDATION_STATUS_FILE = 'status.txt'
+
+DATA_VALIDATION_ALL_REQUIRED_FILES = ["train", "valid", "data.yaml"]
+
