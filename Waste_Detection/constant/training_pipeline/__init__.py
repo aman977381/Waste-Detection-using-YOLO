d79b59f0ace149b48c4cc9a6c61fb1d811decfa6
Aman Singh <50145701+aman977381@users.noreply.github.com>
2024-04-30 17:46:12 +0530

    Update __init__.py
    Data changed

--- Waste_Detection/constant/training_pipeline/__init__.py
+++ Waste_Detection/constant/training_pipeline/__init__.py
@@ -1,6 +1,6 @@
 """
 Data Ingestion
-"https://drive.google.com/file/d/1nEYaOxDQgdVe6Kzi-tldN25LdCKWP66o/view?usp=drive_link"
+"https://drive.google.com/file/d/1ECfl3dtYyfivY8kYPq7RHUBTjC-2vf61/view?usp=share_link"
 """
 
 ARTIFACTS_DIR: str = "artifacts"
@@ -9,7 +9,7 @@ DATA_INGESTION_DIR_NAME: str = "data_ingestion"
 
 DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
 
-DATA_DOWNLOAD_URL: str = "https://drive.google.com/file/d/1ECfl3dtYyfivY8kYPq7RHUBTjC-2vf61/view?usp=share_link"
+DATA_DOWNLOAD_URL: str = "https://drive.google.com/file/d/1nEYaOxDQgdVe6Kzi-tldN25LdCKWP66o/view?usp=drive_link"
 
 """
 Data Validation
@@ -28,6 +28,6 @@ MODEL_TRAINER_DIR_NAME: str = "model_trainer"
 
 MODEL_TRAINER_PRETRAINED_WEIGHT_NAME: str = "yolov8n.pt"
 
-MODEL_TRAINER_NO_EPOCHS: int = 100
+MODEL_TRAINER_NO_EPOCHS: int = 200
 
-MODEL_TRAINER_BATCH_SIZE: int = 16
+MODEL_TRAINER_BATCH_SIZE: int = 64
