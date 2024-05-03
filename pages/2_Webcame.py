f13dd8187f1bc1813adb98c762f6f441e532fcdc
Code_Captain <50145701+aman977381@users.noreply.github.com>
2024-05-03 22:43:08 +0530

    Livecam feature shifted to webrtc

--- pages/2_Webcame.py
+++ pages/2_Webcame.py
@@ -1,7 +1,9 @@
 import cv2
 import math
+import av
 import numpy as np
 import streamlit as st
+from streamlit_webrtc import webrtc_streamer, RTCConfiguration
 from PIL import Image
 from ultralytics import YOLO
 
@@ -27,48 +29,31 @@ background_image = """
 
 st.markdown(background_image, unsafe_allow_html=True)
 
-def predict(img):
+
+def predict(frame):
+    img = frame.to_ndarray(format="bgr24")
     model = YOLO(r"artifacts\model_trainer\best.pt")
-    results = model(img,stream=True)
+    results = model(img, stream=True)
     classNames = ['BIODEGRADABLE', 'CARDBOARD', 'GLASS', 'METAL', 'PAPER', 'PLASTIC']
     for r in results:
         boxes = r.boxes
         for box in boxes:
             x1, y1, x2, y2 = box.xyxy[0]
-            x1, y1, x2, y2 = int(x1),int(y1),int(x2),int(y2)
-            w, h = x2-x1,y2-y1
+            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+            w, h = x2 - x1, y2 - y1
 
-            cv2.rectangle(img,(x1, y1), (x2, y2),color=(255,0,0), thickness=2)
-            conf = math.ceil((box.conf[0]*100))/100
+            cv2.rectangle(img, (x1, y1), (x2, y2), color=(255, 0, 0), thickness=2)
+            conf = math.ceil((box.conf[0] * 100)) / 100
             cls = box.cls[0]
             print(conf)
             name = classNames[int(cls)]
 
-            cv2.putText(img,f"{name} {conf}",(max(0,x1), max(35,y1)),cv2.FONT_HERSHEY_DUPLEX,0.5,(0,150,0),1)
-    return img
+            cv2.putText(img, f"{name} {conf}", (max(0, x1), max(35, y1)), cv2.FONT_HERSHEY_DUPLEX, 0.5,
+                            (0, 150, 0), 1)
+    return av.VideoFrame.from_ndarray(img, format="bgr24")
 
-def main_loop():
-    cap = cv2.VideoCapture(0)
-    frame_placeholder = st.empty()
-    with col1: 
-        start_button = st.button("Start")
-    with col3:
-        stop_button = st.button("Stop")
-    if start_button:
-        while cap.isOpened() and not stop_button:
-            ret, frame = cap.read()
-            if not ret:
-                st.write("Video Capture Ended")
-                break
-            img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            predict_img = predict(img)
-            with col2:
-                frame_placeholder.image(predict_img,channels="RGB")
-            if cv2.waitKey(1) & 0xFF == ord("q") or stop_button:
-                break
-        cap.release()
-        cv2.destroyAllWindows()
 
-col1, col2, col3 = st.columns([1,6,1])
-if __name__ == "__main__":
-    main_loop()
+webrtc_streamer(key='key', video_frame_callback=predict,
+                rtc_configuration=RTCConfiguration(
+                    {"iceServers":[{"urls":["stun:stun.l.google.com:19302"]}]}
+                ))
